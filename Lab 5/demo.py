4669c30a50112c525345e04c8fbe1a348a13153a
JoeD016 <61925885+JoeD016@users.noreply.github.com>
2021-11-07 12:59:53 -0500

    Update demo.py

--- Lab 5/demo.py
+++ Lab 5/demo.py
@@ -48,6 +48,10 @@ while True:
         # cv2.circle(img, (pinkyX, pinkyY), 15, (255, 0, 255), cv2.FILLED)
         # cv2.line(img, (thumbX, thumbY), (pointerX, pointerY), (255, 0, 255), 3)
         # cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        random_x = math.random() * 500
+        random_y = math.random() * 500 
+        cv2.cycle(img,(random_x,random_y), 50, (255,255,0), cv2.FILLED) 
         index_x = (thumbX + pointerX + middleX + ringX + pinkyX + cx) // 6
         idnex_y = (thumbY + pointerY + middleY + ringY + pinkyY + cy) // 6
         cv2.circle(img, (index_x, idnex_y), 50, (255, 0, 255), cv2.FILLED)
