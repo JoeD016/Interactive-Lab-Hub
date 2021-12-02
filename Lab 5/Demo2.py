392085e8a340717ff6306dca46ea3517fa2b228d
JoeD016 <61925885+JoeD016@users.noreply.github.com>
2021-12-01 23:24:39 -0500

    Update Demo2.py

--- Lab 5/Demo2.py
+++ Lab 5/Demo2.py
@@ -93,11 +93,11 @@ while True:
     #             1, (255, 0, 0), 3)
  
  
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
+    #cTime = time.time()
+    #fps = 1 / (cTime - pTime)
+    #pTime = cTime
+    #cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+    #            1, (255, 0, 0), 3)
  
-    cv2.imshow("Img", img)
-    cv2.waitKey(1)
+    #cv2.imshow("Img", img)
+    #cv2.waitKey(1)
